37d7cdcddba200f3b5d030a842f0f61df05b1616
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-07 17:51:54 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -8,13 +8,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  select avg(gpa) from students
+  execute_sql(select avg(gpa) from students)
 end
 
 def total_tardies_for_all_students
-  select sum(tardies) from students
+  execute_sql(select sum(tardies) from students)
 end
 
 def average_gpa_for_9th_grade
-  select avg(gpa) from students where grade = 9
+  execute_sql(select avg(gpa) from students where grade = 9)
 end
